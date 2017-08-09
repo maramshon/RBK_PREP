820b9d8323b6330a47a31e854cb2d3a5cdf23f9e
maramshon <mralshon@yahoo.com>
2017-08-09 09:28:04 +0300

    My first commit

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+// Write a function called RepeatN that accpets and string and a number the return that string n times
+
+function RepeatN(string, n) {
+ return new Array(n+1).join (string+" ");
+}
+
+
+RepeatN('RBK', 3); // => RBK RBK RBK
+RepeatN('Javascript', 5); // => Javascript Javascript Javascript Javascript Javascript
